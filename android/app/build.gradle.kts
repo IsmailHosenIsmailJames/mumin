0f1a0004c1f3f2864ed289e50bcda89e5b3b5d53
MD. ISMAIL HOSEN JAMES <md.ismailhosenismailjames@gmail.com>
2025-11-17 19:11:09 +0600

    [upgrade] package update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mumin"
+    namespace = "com.impala.mumin"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -31,10 +31,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mumin"
+        applicationId = "com.impala.mumin"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
